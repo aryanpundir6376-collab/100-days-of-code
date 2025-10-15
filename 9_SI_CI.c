a93ded76e63eb01e64fffce82564f65ae1b23bfc
aryanpundir6376-collab <aryanpundir6376@gmail.com>
2025-10-15 15:11:37 +0530

    Day 5 Ques 1
    Day 5 Ques 1

---
+++ 9_SI_CI.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    float p,r,t;
+    printf("ENTER PRINCIPAL VALUE, RATE AND TIME \n");
+    scanf("%f %f %f",&p,&r,&t);
+
+    float SI=0,CI=0,C_I=0;
+    SI=(p*r*t)/100.0;
+    CI=p*pow(1+(r/100),t);
+    C_I=CI-p;
+    printf("SIMPLE INTEREST : %f \n",SI); 
+    printf("COMPOUND INTEREST : %f \n",C_I);
+    
+    return 0;
+}
