225d67e616ad2d0a707d7b4f73976c9223c265b0
aryanpundir6376-collab <aryanpundir6376@gmail.com>
2025-10-15 11:20:58 +0530

    Day 1 ques 1
    Day 1 ques 1

---
+++ 1_Sum.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    printf("ENTER TWO NUMBERS \n");
+    scanf("%d %d",&a,&b);
+
+    printf("The sum of two numbers is : %d \n",(a+b));
+    return 0;
+}
