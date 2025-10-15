292f89993332047875cc8df5c594ec729fb50bec
aryanpundir6376-collab <aryanpundir6376@gmail.com>
2025-10-15 11:21:31 +0530

    Day 1 ques 2
    Day 1 ques 2

---
+++ 2_Cal.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    printf("ENTER TWO NUMBERS \n");
+    scanf("%d %d",&a,&b);
+
+    printf("The sum of two numbers is : %d\n",(a+b));
+    printf("The difference of two numbers is : %d\n",(a-b));
+    printf("The product of two numbers is : %d\n",(a*b));
+    printf("The quotient of two numbers is : %d\n",(a/b));
+    return 0;
+}
