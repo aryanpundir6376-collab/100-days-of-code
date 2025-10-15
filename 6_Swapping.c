5d50709c4f0fea5a2f1cc302eeeba395136d891e
aryanpundir6376-collab <aryanpundir6376@gmail.com>
2025-10-15 15:08:35 +0530

    Day 3 Ques 2
    Day 3 Ques 2

---
+++ 6_Swapping.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("ENTER TWO NUMBERS: \n");
+    scanf("%d %d",&a,&b);
+
+    printf("The values of a and b before swapping is :a=%d and b=%d \n",a,b);
+
+    c=a;
+    a=b;
+    b=c;
+    printf("The values of a and b after swapping is :a=%d and b=%d \n",a,b);
+    return 0;
+}
