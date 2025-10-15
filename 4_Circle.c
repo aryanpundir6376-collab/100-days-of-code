3064a24fa43b5d1f6cc80b3d7dce8ed09289f937
aryanpundir6376-collab <aryanpundir6376@gmail.com>
2025-10-15 15:06:46 +0530

    Day 2 Ques 2
    Day 2 Ques 2

---
+++ 4_Circle.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    float r;
+    printf("ENTER RADIUS OF A CIRCLE\n");
+    scanf("%f",&r);
+    printf("THE AREA OF A CIRCLE IS: %f \n",(3.14*r*r));
+    printf("THE CIRCUMFERENCE OF A CIRCLE IS :%f \n",(2*3.14*r));
+    return 0;
+}
