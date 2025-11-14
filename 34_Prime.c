2b33a38bef978ce76ccf0423a32b9232eff44418
aryanpundir6376-collab <aryanpundir6376@gmail.com>
2025-11-14 13:42:19 +0530

    Day 17 Ques 2

---
+++ 34_Prime.c
@@ -0,0 +1,22 @@
+//Write a program to check if a number is prime.
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Enter a number.\n");
+    scanf("%d", &n);
+    
+    int c = 0;
+
+    for (int i = 1; i <= n; i++)
+    {
+        if (n % i == 0)
+            c++;
+    }
+    if (c == 2)
+        printf("Prime number.\n");
+    else
+        printf("Not a Prime number.\n");
+
+    return 0;
+}
