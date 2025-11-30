e2c8d5d98ba372123c0a79a98d0397505eca03a2
aryanpundir6376-collab <aryanpundir6376@gmail.com>
2025-11-30 23:53:57 +0530

    Day 85

---
+++ 135_Ques.c
@@ -0,0 +1,32 @@
+// Assign explicit values starting from 10 and print them.
+#include <stdio.h>
+
+enum Status
+{
+    SUCCESS = 10,
+    FAILURE,
+    TIMEOUT
+};
+
+int main()
+{
+    enum Status s;
+
+    for (s = SUCCESS; s <= TIMEOUT; s++)
+    {
+        switch (s)
+        {
+        case SUCCESS:
+            printf("SUCCESS = %d\n", s);
+            break;
+        case FAILURE:
+            printf("FAILURE = %d\n", s);
+            break;
+        case TIMEOUT:
+            printf("TIMEOUT = %d\n", s);
+            break;
+        }
+    }
+
+    return 0;
+}
