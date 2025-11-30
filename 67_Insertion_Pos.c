71afeb0a89e9e7a96076972a97137f3b83b3da90
aryanpundir6376-collab <aryanpundir6376@gmail.com>
2025-11-30 22:59:27 +0530

    Day 34 Ques 1

---
+++ 67_Insertion_Pos.c
@@ -0,0 +1,33 @@
+//Insert an element in an array at a given position.
+#include <stdio.h>
+
+int main() {
+    int n, i, x, pos;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n + 1];
+
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    printf("Enter element to insert: ");
+    scanf("%d", &x);
+
+    printf("Enter position to insert (1 to %d): ", n + 1);
+    scanf("%d", &pos);
+
+    for (i = n; i >= pos; i--)
+        arr[i] = arr[i - 1];
+
+    arr[pos - 1] = x;
+    n++;
+
+    printf("Array after insertion:\n");
+    for (i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
